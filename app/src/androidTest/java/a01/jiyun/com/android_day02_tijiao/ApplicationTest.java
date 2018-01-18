7652498ff35be75ef3ddd51b55a8d88b6a49affe
m <1459449180@qq.com>
2018-01-18 15:58:04 +0800

    提交

---
+++ app/src/androidTest/java/a01/jiyun/com/android_day02_tijiao/ApplicationTest.java
@@ -0,0 +1,13 @@
+package a01.jiyun.com.android_day02_tijiao;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/a01/jiyun/com/android_day02_tijiao/MainActivity.java
@@ -0,0 +1,13 @@
+package a01.jiyun.com.android_day02_tijiao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
